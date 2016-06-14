7f6c021f892b8772d2d2780e464c817a9b3f01a7
Pea Lutz <pea@glocal.coop>
2016-06-14 16:07:25 -0400

    Initial commit

---
+++ civicrm-wp-directory.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     CiviCRM to WordPress Directory
+ * Plugin URI:      https://github.com/glocalcoop/civicrm-to-wp-directory
+ * Description:     Display CiviCRM contact data in WordPress.
+ * Author:          Glocal
+ * Author URI:      https://glocal.coop
+ * Text Domain:     civicrm-wp-directory
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Civicrm_Wp_Directory
+ */
